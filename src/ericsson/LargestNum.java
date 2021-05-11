eb297df534e452889537820831be82c58ecb2b6e
Kevin Niland <kevinniland97@gmail.com>
2021-05-11 17:41:45 +0100

    Update LargestNum.java

--- src/ericsson/LargestNum.java
+++ src/ericsson/LargestNum.java
@@ -1,5 +1,9 @@
 package ericsson;
 
 public class LargestNum {
-
+    // Forgot to upload this, and just factory reset my laptop so I don't have it any more but look at this:
+    // https://www.knowsh.com/Notes/250501/Maximum-Possible-Value-By-Inserting-5
+    
+    // Can't find a Java solution but should be able to make one from the above - make sure you account for a negative
+    // because that's where I fell down in mine
 }

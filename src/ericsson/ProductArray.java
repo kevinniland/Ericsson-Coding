63c0cd6a0a5ad759c1a7740e30f84e4bd2a196a0
Kevin Niland <kevinniland97@gmail.com>
2021-05-11 17:42:41 +0100

    Create ProductArray.java

---
+++ src/ericsson/ProductArray.java
@@ -0,0 +1,6 @@
+// Forgot to upload this, and just factory reset my laptop so I don't have it any more but the question was:
+
+// Given an array, return a 1, 0, or -1 depending on the product of that array
+// [3, 4, 6] = The product of this array is positive so return 1
+// [-3, 2, 1] = The product of this array is negative so return -1
+// Can't quite remember the 0 but I think if the product is 0, return 0
